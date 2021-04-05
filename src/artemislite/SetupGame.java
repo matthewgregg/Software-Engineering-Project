3e816847c2a3cd7c850b9e2093c88046437c6375
Mark Graham <mgraham54@qub.ac.uk>
2021-04-05 16:50:19 +0100

    Started working on playerNameCheck() method

--- src/artemislite/SetupGame.java
+++ src/artemislite/SetupGame.java
@@ -13,8 +13,11 @@ public class SetupGame {
 	private static final int NUMBER_OF_SQUARES = 12;
 	private ArrayList<Square> squares;
 
+	// moved this out of playerCreation so I could access it in the playerNameCheck()
+	List<Player> players;
+
 	public List<Player> playerCreation(Scanner scanner) {
-		List<Player> players = new ArrayList<>();
+		players = new ArrayList<>();
 
 		try {
 			String userOption = "";
@@ -43,16 +46,30 @@ public class SetupGame {
 				String name = scanner.nextLine();
 				//TODO mix up playerIDs?
 				Player player = new Player(playerID, name);
+				// Added the playerNameCheck before adding the new player to the array!
+				// we could probably incorporate new Player Constructor into it like this 
+				// playerNameCheck(new Player(playerID, name)), but I haven't investigated that approach!
+				playerNameCheck(name, playerID);
 				players.add(player);
 				playerID++;
+				
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		return players;
 	}
-
-	public void playerNameCheck() {
+	// Just have a play with handling the name check needs work!
+	// I only spent 15 minutes to break up my revision - if anyone wants to progress it feel free!
+	public void playerNameCheck(String name, int playerID) {
+		//players = new ArrayList<Player>();
+		
+		for(Player player: players) {
+			if(player.getName().equalsIgnoreCase(name) || player.getPlayerID() == playerID) {
+				System.out.println("Do something with this Duplicate " + name);
+				System.out.println("Do something with this Duplicate " + playerID);
+			}
+		}
 
 	}
 
