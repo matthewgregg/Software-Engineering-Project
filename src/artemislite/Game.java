11e3c2c5ba340d5f81ec02f8bc33057f0f0d6fe8
40084448 <dkieran04@qub.ac.uk>
2021-04-06 18:18:21 +0100

    Added traverseBoard() and split rollDice() into 2 seperate methods

--- src/artemislite/Game.java
+++ src/artemislite/Game.java
@@ -8,10 +8,10 @@ public class Game {
 	private static final Random rand = new Random();
 	private static List<Player> players;
 	private static final Scanner scanner = new Scanner(System.in);
-	//scanner cannot be closed and then reused
+	// scanner cannot be closed and then reused
 
 	SetupGame gameSetup = new SetupGame();
-	
+
 	public static void main(String[] args) {
 		clearScreen();
 		boolean isGameOver = false;
@@ -19,7 +19,8 @@ public class Game {
 
 		players = new ArrayList<>(SetupGame.playerCreation(scanner));
 
-		//squares will be moved to a player's array when purchased and replaced with null in this array
+		// squares will be moved to a player's array when purchased and replaced with
+		// null in this array
 		ArrayList<Square> squares = new ArrayList<>(SetupGame.setupBoard());
 
 		clearScreen();
@@ -38,7 +39,7 @@ public class Game {
 				playerCount = 1;
 			}
 			quitGame = !generateOptionsMenu(scanner, player, squares);
-		} while(!isGameOver && !quitGame);
+		} while (!isGameOver && !quitGame);
 
 		if (quitGame) {
 			clearScreen();
@@ -77,9 +78,10 @@ public class Game {
 				if (ownedSquare.getFirst().equals(player)) {
 					System.out.printf("You are on %s. You own it.", squareName);
 				} else {
-					System.out.printf("You are on %s. It is owned by %s.\n", squareName, ownedSquare.getFirst().getName());
+					System.out.printf("You are on %s. It is owned by %s.\n", squareName,
+							ownedSquare.getFirst().getName());
 				}
-			} else if (!onSysSq){
+			} else if (!onSysSq) {
 				System.out.printf("You are on %s. It can't be owned.\n", landed.getSquareName());
 			} else {
 				System.out.printf("You are on %s. It is not owned.\n", landed.getSquareName());
@@ -99,7 +101,7 @@ public class Game {
 				}
 				menuNum++;
 				System.out.println(menuNum + ". " + allMenu[i]);
-				menuOptions.put(menuNum, i+1);
+				menuOptions.put(menuNum, i + 1);
 			}
 			System.out.println("Enter option");
 
@@ -123,57 +125,126 @@ public class Game {
 			clearScreen();
 
 			switch (menuOptions.get(userOption)) {
-				case 1:
-					displayBoardState();
-					loading();
-					break;
-				case 2:
-					rolled = true;
-					int[] roll = rollDice();
-					System.out.printf("You rolled a %d and a %d.\nMoving %d spaces", roll[0], roll[1], roll[0]+roll[1]);
-					loading();
-					break;
-				case 3:
-					Square square = unownedSquares.get(player.getPosition());
-					if (square instanceof SystemSquare) {
-						SystemSquare ss = (SystemSquare) square;
-						ss.purchaseSquare(player);
-						System.out.print("Purchasing " + ss.getSquareName());
-					} else {
-						System.out.println("That square can't be purchased.");
-					}
-					loading();
-					break;
-				case 4:
-					System.out.print("Auction");
-					//this should happen automatically if player can't buy development
-					loading();
-					break;
-				case 5:
-					System.out.print("Develop");
-					loading();
-					break;
-				case 6:
-					turnFinished = true;
-					break;
-				case 7:
-					break;
+			case 1:
+				displayBoardState();
+				loading();
+				break;
+			case 2:
+				rolled = true;
+				int[] roll = rollDiceToMove(player);
+				System.out.printf("You rolled a %d and a %d.\nMoving %d spaces", roll[0], roll[1], roll[0] + roll[1]);
+				System.out.printf("You are now sitting at Square %s, what would you like to do?", player.getPosition());
+				loading();
+				break;
+			case 3:
+				Square square = unownedSquares.get(player.getPosition());
+				if (square instanceof SystemSquare) {
+					SystemSquare ss = (SystemSquare) square;
+					ss.purchaseSquare(player);
+					System.out.print("Purchasing " + ss.getSquareName());
+				} else {
+					System.out.println("That square can't be purchased.");
+				}
+				loading();
+				break;
+			case 4:
+				System.out.print("Auction");
+				// this should happen automatically if player can't buy development
+				loading();
+				break;
+			case 5:
+				System.out.print("Develop");
+				loading();
+				break;
+			case 6:
+				turnFinished = true;
+				break;
+			case 7:
+				break;
 			}
 		} while (!turnFinished && userOption != menuNum);
 		return turnFinished;
 	}
 
-	public static int[] rollDice() {
+	/**
+	 * Roll dice to see who goes first, second, third etc
+	 * 
+	 * @return
+	 */
+	public static int[] rollDiceForPlayerOrder() {
 		int[] roll = new int[2];
 		roll[0] = rand.nextInt(6) + 1;
 		roll[1] = rand.nextInt(6) + 1;
-		
+
 		return roll;
+	}
+
+	/**
+	 * Roll dice to enable players to move on board
+	 * 
+	 * @return
+	 */
+	public static int[] rollDiceToMove(Player player) {
+		int[] roll = new int[2];
+		roll[0] = rand.nextInt(6) + 1;
+		roll[1] = rand.nextInt(6) + 1;
+
+		traverseBoard(roll, player);
+		return roll;
+	}
+
+	/**
+	 * moves players' position on board based on result of dice roll
+	 */
+	public static void traverseBoard(int[] rolledNumber, Player player) {
+		
+		switch (rolledNumber[0]+rolledNumber[1]) {
+		case 0:
+			player.setPosition(0);
+			break;
+		case 1:
+			player.setPosition(1);
+			break;
+		case 2:
+			player.setPosition(2);
+			break;
+		case 3:
+			player.setPosition(3);
+			break;
+		case 4:
+			player.setPosition(4);
+			break;
+		case 5:
+			player.setPosition(5);
+			break;
+		case 6:
+			player.setPosition(6);
+			break;
+		case 7:
+			player.setPosition(7);
+			break;
+		case 8:
+			player.setPosition(8);
+			break;
+		case 9:
+			player.setPosition(9);
+			break;
+		case 10:
+			player.setPosition(10);
+			break;
+		case 11:
+			player.setPosition(11);
+			break;
+		case 12:
+			player.setPosition(12);
+			break;
+		}
+		
 	}
 
 	public static void loading() {
 		try {
-			for (int i=0; i<=3;i++) {
+			for (int i = 0; i <= 3; i++) {
 				Thread.sleep(1000);
 				System.out.print(".");
 			}
@@ -185,12 +256,12 @@ public class Game {
 
 	public static StringBuilder welcomeMessage(List<Player> players) {
 		StringBuilder welcome = new StringBuilder();
-		for(int i = players.size(); i > 0; i--) {
-			welcome.insert(0, players.get(i-1).getName());
+		for (int i = players.size(); i > 0; i--) {
+			welcome.insert(0, players.get(i - 1).getName());
 			if (i == players.size()) {
 				welcome.insert(0, " and ");
 			} else if (i > 1) {
-				welcome.insert(0,", ");
+				welcome.insert(0, ", ");
 			}
 		}
 		welcome.append(".");
@@ -242,8 +313,7 @@ public class Game {
 				System.out.print("\033[H\033[2J");
 				System.out.flush();
 			}
-		}
-		catch (final Exception e) {
+		} catch (final Exception e) {
 			System.out.println("Error clearing console.");
 		}
 	}
