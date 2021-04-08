b1024952cbed3621247a72243fb7e5de3302b2cf
Mark Graham <mgraham54@qub.ac.uk>
2021-04-08 13:05:13 +0100

    Created a MiniGame() class to store our mini games

---
+++ src/artemislite/MiniGames.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package artemislite;
+
+/**
+ * @author mark
+ *
+ */
+public class MiniGames {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO The main method is just here to test your game!
+
+	}
+	
+	public void navigationGame() {
+		
+		// TODO - Thought this could be a simple math game!
+		
+		
+		/**
+		 * Example
+		 * 
+		 * Q1. You are traveling at X speed what distance will you travel by X distance??
+		 * 
+		 * A. wrong answer
+		 * B. correct answer 
+		 * C. wrong answer
+		 * D. wrong answer
+		 * 
+		 * 
+		 * Q2. What is X * Y - Z?
+		 * A. correct answer
+		 * 
+		 * Add up all the correct answers up -> if you answer over 60% you win the game and get a major development
+		 * 
+		 * 
+		 * return winner/loser;
+		 * 
+		 */
+		
+		
+	}
+	
+	
+	public void whatIsArtemisGame() {
+		
+		// TODO - Thought this could be simple questions about the Artemis Project - Could also have another game about the History of NASA
+		
+		/**
+		 * Example
+		 * 
+		 * Q1. What is the name of the docking module?
+		 * 
+		 * A. LUNAR
+		 * B. ORION
+		 * C. SLS
+		 * D. EXPLORER
+		 * 
+		 * 
+		 * return winner/loser;
+		 */
+		
+		
+	}
+
+	// Other Games ... ... 
+	
+}
