b7b505c8989ffa70e19116d90efed9fdcf1e22d0
Mark Graham <mgraham54@qub.ac.uk>
2021-04-15 10:43:10 +0100

    created a nasa mini game

--- src/artemislite/MiniGames.java
+++ src/artemislite/MiniGames.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
+import artemis.lite.game.app.MultiChoiceQuiz;
+
 /**
  * @author mark
  *
@@ -20,6 +22,15 @@ public class MiniGames {
 	 */
 
 	public static void main(String[] args) {
+		
+		// nasa game
+		
+		if(nasaQuiz() == true) {
+			System.out.println("Congrats! You have won your major development!");
+		} else {
+			System.out.println("Try again loser!");
+		}
+				
 
 		Map<String, String> map = new HashMap<>();
 		map.put("How many months are there in 12 years?\n a) 124\n b) 142\n c) 144\n", "c");
@@ -61,6 +72,72 @@ public class MiniGames {
 		System.out.println("Your Score is " + ans);
 
 	}
+	
+	public static boolean nasaQuiz() {
+		
+		MultiChoiceQuiz q1 = new MultiChoiceQuiz("NASA stands for the National ____ and Space Administration. Fill in the blank.", "Astronomy", "Aeronautics", "Acrobatic", "Airplane", "B");
+		MultiChoiceQuiz q2 = new MultiChoiceQuiz("When was NASA founded?", "1945", "1958", "1969", "1980", "B");
+		MultiChoiceQuiz q3 = new MultiChoiceQuiz();
+		q3.setQuestion("True or false? The first person in space was an American.");
+		q3.setChoice1("True");
+		q3.setChoice2("False");
+		q3.setCorrectAnswer("B");
+		MultiChoiceQuiz q4 = new MultiChoiceQuiz("NASA quarantined the astronauts who landed on the moon when they returned to Earth?.Why?", "Moon germs", "Rest", "Adjusting to gravity", "Dizziness", "A");
+		MultiChoiceQuiz q5 = new MultiChoiceQuiz("Which of these is not an Apollo command module name?", "Garfield", "Gumdrop", "Charlie Brown", "Casper", "A");
+
+		ArrayList<MultiChoiceQuiz> questions = new ArrayList<MultiChoiceQuiz>();
+		questions.add(q1);
+		questions.add(q2);
+		questions.add(q3);
+		questions.add(q4);
+		questions.add(q5);
+
+		int score = 0;
+		Scanner userInput = new Scanner(System.in);
+		boolean correctInput = false;
+		
+		System.out.println(" ");
+		
+		for (MultiChoiceQuiz question : questions) {
+
+			System.out.println("Question : "  + question.getQuestion() + "\n");
+			System.out.println("\t A. " + question.getChoice1());
+			System.out.println("\t B. " + question.getChoice2());
+			if(question.getChoice3() != null) {
+				System.out.println("\t C. " + question.getChoice3());
+			}
+			if(question.getChoice4() != null) {
+				System.out.println("\t D. " + question.getChoice4());
+			}
+			// TODO - if a question only has 2 choices a business rule needs added to stop the use of "C" & "D" as options.
+			do {
+				String answer = userInput.nextLine();
+				if (answer.equalsIgnoreCase("A") || answer.equalsIgnoreCase("B") || answer.equalsIgnoreCase("C") || answer.equalsIgnoreCase("D")) {
+					if (answer.equalsIgnoreCase(question.getCorrectAnswer())) {
+						score++;
+						correctInput = true;
+					}
+				} else {
+					System.out.println("You have entered the wrong character");
+				}
+			} while (!correctInput);
+			
+		}
+
+
+		System.out.println("You got " + score + "/" + questions.size());
+		
+		if(score >= questions.size() - 2) {
+			return true;
+		}
+		return false;
+		
+		
+		// System.out.println("Questions by www.nationalgeographic.com/science");
+		
+	}
+	
+	
 	/*
 	 * //level 1 of minigame difficulty easy questions HashMap<String, String> quiz1
 	 * = new HashMap<>();
